385a248eef947daca3a5fcf1884ab25f3bbd7fc8
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-22 16:23:14 +0400

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer({ isDark }: { isDark: boolean }) {
   return (
     <div className="flex justify-center items-center mt-[4rem]">
-      <p className="text-[#9495a5] text-[1.4rem] font-[400] tracking-[-0.194px]">
+      <p
+        className={`text-[1.4rem] font-[400] tracking-[-0.194px] ${
+          !isDark ? "text-[#9495a5]" : "text-[#5b5e7e]"
+        }`}
+      >
         Drag and drop to reorder list
       </p>
     </div>
